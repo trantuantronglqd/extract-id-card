019c5f8197123e4b180d3796a2996a4e0e4b9ff9
trantuantronglqd <trantuantronglqd@gmail.com>
2023-06-30 04:27:40 +0700

    first extract demo

---
+++ modules.py
@@ -0,0 +1,23 @@
+from email import message
+from flask import Flask, render_template, request, jsonify
+from werkzeug.utils import secure_filename
+import align
+
+import os
+import colorsys
+
+import numpy as np
+from keras import backend as K
+from keras.layers import Input
+
+from yolo4.model import yolo_eval, yolo4_body
+from yolo4.utils import letterbox_image
+
+import tensorflow as tf
+tf.compat.v1.disable_v2_behavior()
+
+from vietocr.tool.predictor import Predictor
+from vietocr.tool.config import Cfg
+
+from PIL import Image, ImageFont, ImageDraw
+import cv2
